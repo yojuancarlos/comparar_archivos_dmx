53e1ab100f867743d0dd0c865dc4ff47ff7fbc02
yojuancarlos <56267375+yojuancarlos@users.noreply.github.com>
2022-04-19 13:03:50 -0500

    agregue la solicitud de Conocer los archivos DMX que poseen el mismo nombre y que sus pesos sean iguales

--- rutas.py
+++ rutas.py
@@ -2,78 +2,67 @@ import json
 import os
 
 
-#-------------------------------------------------------------------
-#aqui van las rutas
+# -------------------------------------------------------------------
+# aqui van las rutas
 carpeta1 = "C:\pruebas\carpeta1"
-carpeta2="C:\pruebas\carpeta2"
+carpeta2 = "C:\pruebas\carpeta2"
 
 
-list_Carpeta1= os.listdir(carpeta1)
-list_Carpeta2= os.listdir(carpeta2)
+list_Carpeta1 = os.listdir(carpeta1)
+list_Carpeta2 = os.listdir(carpeta2)
 
-#conjunto
-m=set(list_Carpeta1)
-
-m2=set(list_Carpeta2)
-#---------------------------------------------------------------------------------
-#utilizo teoria de conjuntos
-#archivos repetidos
+# conjunto
+m = set(list_Carpeta1)
+m2 = set(list_Carpeta2)
+# ---------------------------------------------------------------------------------
+# utilizo teoria de conjuntos
+# archivos repetidos
 archivosrepetidos=m&m2
 
 
-#archivos que estan en una carpeta pero no en otra
-archivosdiferentes=m-m2
-archivosdiferentes2=m2-m
+# archivos que estan en una carpeta pero no en otra
+archivosdiferentes = m-m2
+archivosdiferentes2 = m2-m
 
 
 #    diferencia    "-"
 
-#--------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------
 # aqui mero el peso de cada archivo
-
-listaprueba=[]
-rutas=[]
-peso=[]
-
-diccionarioprueba={}
+archivos_con_pesos_iguales=[]
+rutasypeso=[]
 for filename in archivosrepetidos:
- #   --------------------------
-    #print("---"*20)
     variable_de_rutas_de_la_carpeta1 =(f"{carpeta1}\{filename}")
     file_size = os.path.getsize(variable_de_rutas_de_la_carpeta1)
     variable_de_rutas_de_la_carpeta2 = (f"{carpeta2}\{filename}")
     file_size2 = os.path.getsize(variable_de_rutas_de_la_carpeta2)
-#-----------------------------------------------------------------------------------------------------
-#aqui miro cual es el mas pesado cuando los archivos se repiten
+# -----------------------------------------------------------------------------------------------------
+# aqui miro cual es el mas pesado cuando los archivos se repiten
     if file_size > file_size2:
 
-        #print(f"el archivo {filename} mas pesados esta en la {variable_de_rutas_de_la_carpeta1}  y su peso es {file_size}")
-        listaprueba.append((variable_de_rutas_de_la_carpeta1,file_size))
-
-
+        # print(f"el archivo {filename} mas pesados esta en la {variable_de_rutas_de_la_carpeta1}  y su peso es {file_size}")
+        rutasypeso.append((variable_de_rutas_de_la_carpeta1,file_size))
     elif file_size2 > file_size:
        # print(f"el archivo {filename} mas pesado esta en la {variable_de_rutas_de_la_carpeta2}  y su peso es {file_size2}")
 
-        listaprueba.append((variable_de_rutas_de_la_carpeta2,file_size2))
+        rutasypeso.append((variable_de_rutas_de_la_carpeta2,file_size2))
+    if file_size== file_size2:
+       archivos_con_pesos_iguales.append(filename)
 
 
-print(listaprueba)
-#aqui aplico el json
+print(rutasypeso)
+# aqui aplico el json
 
-#
 
 
 
 diccionario_dearchivos_archivosrepetidos={'ambas_carpetas':list(archivosrepetidos),
                                           'solo_carpeta1':list(archivosdiferentes),
-                                          'solo_Carpeta2':list(archivosdiferentes2)
-
-
-
-}
-#aqui adiciono a json el peso
+                                          'archivos_iguales':list(archivos_con_pesos_iguales),
+                                          'solo_Carpeta2':list(archivosdiferentes2)}
+# aqui adiciono a json el peso
 diccionario_dearchivos_archivosrepetidos['archivosconpeso']={}
-for elemento in listaprueba:
+for elemento in rutasypeso:
     key=elemento[0]
     value=elemento[1]
     diccionario_dearchivos_archivosrepetidos['archivosconpeso'][key]=value
@@ -83,14 +72,7 @@ for elemento in listaprueba:
 with open('data.json','w') as file:
     json.dump(diccionario_dearchivos_archivosrepetidos, file, indent=4)
 
-#with open('algo.json', 'w') as fp:
-   # json.dump(diccionario_dearchivos_archivosrepetidos, fp)
-# r es lectura
-#with open('algo.json', 'r') as fp:
- #   data = json.load(fp)
-#print(data)
-#print(type(data))
-#----------------
+
 
 
 
