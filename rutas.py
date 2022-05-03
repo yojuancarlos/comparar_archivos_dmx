4db1d57943969dbbb7e41d1b97e938a730c416f7
yojuancarlos <56267375+yojuancarlos@users.noreply.github.com>
2022-05-02 19:07:09 -0500

    realize algunas mejoras en la parte estetica

--- rutas.py
+++ rutas.py
@@ -1,96 +1,64 @@
 import json
 import os
 import re
-
-
 # -------------------------------------------------------------------
 # aqui van las rutas
 carpeta1 = "C:\pruebas\carpeta1"
 carpeta2 = "C:\pruebas\carpeta2"
 
-
 lista_carpeta1 = os.listdir(carpeta1)
 lista_carpeta2 = os.listdir(carpeta2)
-exprecion_regular="(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])([0-1][0-9]|2[0-3])([0-5][0-9])\.dmx$"
+exprecion_regular = "(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])([0-1][0-9]|2[0-3])([0-5][0-9])\.dmx$"
 # conjunto
 m = set(lista_carpeta1)
 m2 = set(lista_carpeta2)
 # ---------------------------------------------------------------------------------
 # utilizo teoria de conjuntos
 # archivos repetidos
-archivosrepetidos=m&m2
-
-
+archivosrepetidos= m & m2
 # archivos que estan en una carpeta pero no en otra
-archivosdiferentes = m-m2
-archivosdiferentes2 = m2-m
-
-
+archivosdiferentes = m - m2
+archivosdiferentes2 = m2 - m
 #    diferencia    "-"
 # para que solo me muestre archivos dmx
 # --------------------------------------------------------------------------------------------------
 # filtrar archivos dmx
-
 archivos_dmx_repetidos = [f for f in archivosrepetidos if re.match(exprecion_regular, f)]
-print(archivos_dmx_repetidos)
 archivos_dmx_diferentes = [f for f in archivosdiferentes if re.match(exprecion_regular, f)]
-print(archivos_dmx_diferentes)
 archivos_dmx_diferentes2 = [f for f in archivosdiferentes2 if re.match(exprecion_regular, f)]
-print(archivos_dmx_diferentes2)
-
-
 # --------------------------------------------------------------------------------------------------
 # aqui mero el peso de cada archivo
-archivos_con_pesos_iguales=[]
-rutasypeso=[]
-
+archivos_con_pesos_iguales = []
+rutasypeso = []
 
 for filename in archivos_dmx_repetidos:
-    #variable_de_rutas_de_la_carpeta1 =(f"{carpeta1}\{filename}")
     variable_de_rutas_de_la_carpeta1 = os.path.join(carpeta1,filename)
-    #print(variable_de_rutas_de_la_carpeta1)
     peso_archivo_carpeta1 = os.path.getsize(variable_de_rutas_de_la_carpeta1)
-    #variable_de_rutas_de_la_carpeta2 = (f"{carpeta2}\{filename}")
     variable_de_rutas_de_la_carpeta2 = os.path.join(carpeta2, filename)
-    #print(variable_de_rutas_de_la_carpeta2)
     peso_archivo_carpeta2 = os.path.getsize(variable_de_rutas_de_la_carpeta2)
-
 # -----------------------------------------------------------------------------------------------------
 # aqui miro cual es el mas pesado cuando los archivos se repiten
     if peso_archivo_carpeta1 >peso_archivo_carpeta2:
-
         # print(f"el archivo {filename} mas pesados esta en la {variable_de_rutas_de_la_carpeta1}  y su peso es {peso_archivo_carpeta1}")
         rutasypeso.append((variable_de_rutas_de_la_carpeta1,peso_archivo_carpeta1))
     elif peso_archivo_carpeta2 > peso_archivo_carpeta1:
        # print(f"el archivo {filename} mas pesado esta en la {variable_de_rutas_de_la_carpeta2}  y su peso es {peso_archivo_carpeta12}")
-
         rutasypeso.append((variable_de_rutas_de_la_carpeta2,peso_archivo_carpeta2))
     if peso_archivo_carpeta1==peso_archivo_carpeta2:
        archivos_con_pesos_iguales.append(filename)
-
-
-
 # aqui aplico el json
-
-
-
-
-diccionario_dearchivos_archivosrepetidos={'ambas_carpetas':list(archivos_dmx_repetidos),
+diccionario_de_respuesta={'ambas_carpetas':list(archivos_dmx_repetidos),
                                           'solo_carpeta1':list(archivos_dmx_diferentes),
                                           'archivos_iguales':list(archivos_con_pesos_iguales),
                                           'solo_carpeta2':list(archivos_dmx_diferentes2)}
 # aqui adiciono a json el peso
-diccionario_dearchivos_archivosrepetidos['archivosconpeso']={}
+diccionario_de_respuesta['archivosconpeso']={}
 for elemento in rutasypeso:
     key=elemento[0]
     value=elemento[1]
-    diccionario_dearchivos_archivosrepetidos['archivosconpeso'][key]=value
-
-
-
-
+    diccionario_de_respuesta['archivosconpeso'][key]=value
 with open('data.json','w') as file:
-    json.dump(diccionario_dearchivos_archivosrepetidos, file, indent=4)
+    json.dump(diccionario_de_respuesta, file, indent=4)
 
 
 
